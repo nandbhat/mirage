be944ef8eed2f3659c99b61749f96acd8173755b
nandbhat <nandanbhatd@gmail.com>
2020-11-01 02:31:56 +0530

    Added basic routes and dark theme antd

---
+++ src/scenes/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Button } from "antd";
+import { withRouter } from "react-router-dom";
+import { Paths } from "../utils/constants/paths";
+const Home = ({ history }) => {
+  return (
+    <>
+      <div style={{ display: "grid", height: "100vh", placeItems: "center" }}>
+        <Button
+          type="primary"
+          onClick={() => {
+            history.push(Paths.FACIAL_RECOGNITION);
+          }}
+        >
+          Face Detection
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default withRouter(Home);
---
+++ src/utils/constants/paths.js
@@ -0,0 +1,4 @@
+export const Paths = Object.freeze({
+  HOME: "/",
+  FACIAL_RECOGNITION: "/facial-recognition",
+});
